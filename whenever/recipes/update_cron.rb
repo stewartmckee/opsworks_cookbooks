2c209506fecf54db1259c3b7affbda11b1f39754
Stewart McKee <stewart@theizone.co.uk>
2013-04-28 15:53:49 +0100

    updated to get deploy data

--- whenever/recipes/update_cron.rb
+++ whenever/recipes/update_cron.rb
@@ -6,12 +6,15 @@
 # Set your application name here
 appname = "dailyfarm"
  
-#if node[:fqdn].include?("floccus")
- 
-  execute "whenever" do
-    cwd "#{deploy[:deploy_to]}/current"
-    command "sudo bundle exec whenever --update-crontab '#{appname}'"
-    action :run
+if node[:fqdn].include?("floccus")
+  node[:deploy].each do |application, deploy|
+    
+    execute "whenever" do
+      cwd "#{deploy[:deploy_to]}/current"
+      command "sudo bundle exec whenever --update-crontab '#{appname}'"
+      action :run
+    end
+    
   end
   
-#end
+end
