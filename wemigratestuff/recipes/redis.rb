92b0c4a9bc79c6a7861bee37412efe8a97f9a894
Stewart McKee <stewart@theizone.co.uk>
2015-09-02 00:09:50 +0100

    updates

---
+++ wemigratestuff/recipes/redis.rb
@@ -0,0 +1,11 @@
+node[:deploy].each do |application, deploy|
+  bash "change bind address" do
+    cwd "/etc/redis"
+    code "sed -i 's/bind 127.0.0.1/bind 0.0.0.0' /etc/redis/redis.conf"
+  end
+
+  bash "reload redis" do
+    user "root"
+    code "service restart redis-server"
+  end
+end
--- wemigratestuff/recipes/sidekiq.rb
+++ wemigratestuff/recipes/sidekiq.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
 
-  bash "start sidekiq"
+  bash "start sidekiq" do
     cwd "#{deploy[:deploy_to]}/current"
     code "nohup bundle exec sidekiq -C #{deploy[:deploy_to]}/current/config/sidekiq.yml -e production 2>&1 &"
   end
