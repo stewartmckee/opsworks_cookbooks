f199ab104c9824492c3f09a043b66d48205f45f6
Stewart McKee <stewart@theizone.co.uk>
2015-09-02 11:32:50 +0100

    fixed sed command

--- wemigratestuff/recipes/redis.rb
+++ wemigratestuff/recipes/redis.rb
@@ -1,7 +1,7 @@
 node[:deploy].each do |application, deploy|
   bash "change bind address" do
     cwd "/etc/redis"
-    code "sed -i 's/bind 127.0.0.1/bind 0.0.0.0' /etc/redis/redis.conf"
+    code "sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf"
   end
 
 end
