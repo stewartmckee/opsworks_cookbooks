6ed7a53cfa5f1da6f540723e413487ab67d255c7
Stewart McKee <stewart@theizone.co.uk>
2015-09-03 12:45:32 +0100

    updating location of code

--- wemigratestuff/recipes/restart_sidekiq.rb
+++ wemigratestuff/recipes/restart_sidekiq.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
 
-  bash "restart web server" do
+  bash "restart sidekiq" do
     user "root"
     code "kill -s USR2 `ps aux | grep \"sidekiq\" | grep -v grep | awk {'print$2'}`"
   end
--- wemigratestuff/recipes/worker.rb
+++ wemigratestuff/recipes/worker.rb
@@ -1,4 +1,8 @@
 node[:deploy].each do |application, deploy|
 
+  bash "start sidekiq" do
+    cwd "#{deploy[:deploy_to]}/current"
+    code "nohup bundle exec sidekiq -C #{deploy[:deploy_to]}/current/config/sidekiq.yml -e production > #{deploy[:deploy_to]}/current/log/sidekiq.log  2>&1 &"
+  end
   
 end
