4bd3fc66416810db9e9f78fa2cc72615d4a85fa6
Stewart McKee <stewart@theizone.co.uk>
2017-01-15 01:05:25 +0000

    fixing to production env

--- rails/recipes/precompile_assets.rb
+++ rails/recipes/precompile_assets.rb
@@ -3,6 +3,6 @@ node[:deploy].each do |application, deploy|
   Chef::Log.info("Precompiling assets...")
   execute "rake assets:precompile" do
     cwd "#{deploy[:deploy_to]}/current"
-    command "bundle exec rake assets:precompile"
+    command "RAILS_ENV=production bundle exec rake assets:precompile"
   end
 end
