8a0a6d74a9bd17ae303aa7bec6e6054a4b6714f9
Stewart McKee <stewart@theizone.co.uk>
2017-01-15 13:04:14 +0000

    updating precompile assets

--- walkthru/recipes/web.rb
+++ walkthru/recipes/web.rb
@@ -5,6 +5,22 @@ node[:deploy].each do |application, deploy|
     mode '0777'
   end
 
+  node[:deploy].each do |application, deploy|
+    Chef::Log.info("Precompiling assets...")
+    execute "rake assets:precompile" do
+      cwd "#{deploy[:deploy_to]}/current"
+      command "RAILS_ENV=production bundle exec rake assets:precompile"
+    end
+  end
+
+  node[:deploy].each do |application, deploy|
+    Chef::Log.info("Cleaning assets...")
+    execute "rake assets:clean" do
+      cwd "#{deploy[:deploy_to]}/current"
+      command "RAILS_ENV=production bundle exec rake assets:clean"
+    end
+  end
+
   bash "restart unicorn" do
     user "root"
     code "kill -s USR2 `ps aux | grep \"unicorn_rails master\" | grep -v grep | awk {'print$2'}`"
