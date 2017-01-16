e25cc548ffec1de06e437ac721cd958df6100332
Stewart McKee <stewart@theizone.co.uk>
2017-01-16 21:53:54 +0000

    updated

--- walkthru/recipes/default.rb
+++ walkthru/recipes/default.rb
@@ -1,5 +1,10 @@
 node[:deploy].each do |application, deploy|
 
+  directory "#{deploy[:deploy_to]}/current/config" do
+    action :create
+    mode '0777'
+  end
+
   template "#{deploy[:deploy_to]}/current/config/redis.yml" do
     source "redis.yml.erb"
     owner "root"
--- walkthru/recipes/web.rb
+++ walkthru/recipes/web.rb
@@ -1,8 +1,8 @@
 node[:deploy].each do |application, deploy|
 
-  directory "#{deploy[:deploy_to]}/current/tmp/cache/assets/production" do
-    action :create
-    mode '0777'
+  # directory "#{deploy[:deploy_to]}/current/tmp/cache/assets/production" do
+  #   action :create
+  #   mode '0777'
   end
 
   node[:deploy].each do |application, deploy|
@@ -21,9 +21,9 @@ node[:deploy].each do |application, deploy|
     end
   end
 
-  bash "restart unicorn" do
-    user "root"
-    code "kill -s USR2 `ps aux | grep \"unicorn_rails master\" | grep -v grep | awk {'print$2'}`"
-  end
+  # bash "restart unicorn" do
+  #   user "root"
+  #   code "kill -s USR2 `ps aux | grep \"unicorn_rails master\" | grep -v grep | awk {'print$2'}`"
+  # end
 
 end
